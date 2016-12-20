2a02e966940f11ea2223daa62f0ebc5e1f8dad30
Oddclock <javiergarcia.escav@gmail.com>
2016-12-20 19:53:00 +0100

    Movimiento fútbol 2
    El personaje sigue moviéndose en horizontal (intentando arreglar)

--- Assets/Scripts/Player1_movimiento.cs
+++ Assets/Scripts/Player1_movimiento.cs
@@ -2,8 +2,8 @@
 using System.Collections;
 [RequireComponent (typeof(AudioSource))]
 
-public class Movimiento_player_1 : MonoBehaviour {
-	
+public class Player1_movimiento : MonoBehaviour {
+
 	private Rigidbody2D rb;
 	public float velocid = 10f;
 
@@ -12,13 +12,13 @@ public class Movimiento_player_1 : MonoBehaviour {
 		rb = GetComponent<Rigidbody2D> ();
 
 	}
-		
+
 	// Update is called once per frame
 	void Update () {
 
 		if (Input.GetKey ("right")) {
 			rb.velocity = new Vector2 (velocid, rb.velocity.y);
-		
+
 		} 
 
 		if (Input.GetKeyUp ("right")) {
@@ -27,16 +27,11 @@ public class Movimiento_player_1 : MonoBehaviour {
 
 		} else if (Input.GetKey ("left")) {
 			rb.velocity = new Vector2 (-velocid, rb.velocity.y);
-	
+
 
 		} else if (Input.GetKeyUp ("left")) {
 			rb.velocity = new Vector2 (0, rb.velocity.y);
-		
 
-		}
-
-		//if (Input.GetKeyDown (KeyCode.UpArrow)) {
-			
-			//rb.AddForce (Vector3.up * velocid, rb.velocity.y)
 		}
 	}
+}
---
+++ Assets/Scripts/Player2_movimiento.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent (typeof(AudioSource))]
+
+public class Player2_movimiento : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	public float velocid = 10f;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.D)) {
+			rb.velocity = new Vector2 (velocid, rb.velocity.y);
+
+		} 
+
+		if (Input.GetKeyUp (KeyCode.D)) {
+			rb.velocity = new Vector2 (0, rb.velocity.y);
+
+
+		} else if (Input.GetKey (KeyCode.A)) {
+			rb.velocity = new Vector2 (-velocid, rb.velocity.y);
+
+
+		} else if (Input.GetKeyUp (KeyCode.A)) {
+			rb.velocity = new Vector2 (0, rb.velocity.y);
+		
+		}
+	}
+}
+
